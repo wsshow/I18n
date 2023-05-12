8a04588837e0be989ac9a4d5e8cf1e948bba117c
ws <1479162130@qq.com>
2023-05-12 14:13:41 +0800

    feat: function to get current lang

--- i18n.go
+++ i18n.go
@@ -7,33 +7,33 @@ import (
 	"sync"
 )
 
-type Message struct {
+type message struct {
 	ID    string            `json:"id"`
 	Langs map[string]string `json:"langs"`
 }
 
-type I18n struct {
-	messages map[string]Message
+type i18n struct {
+	messages map[string]message
 	mutex    sync.RWMutex
 	lang     string
 	err      error
 }
 
-func NewI18n() *I18n {
-	return &I18n{
+func NewI18n() *i18n {
+	return &i18n{
 		lang:     "en",
-		messages: make(map[string]Message),
+		messages: make(map[string]message),
 		err:      nil,
 	}
 }
 
-func (i *I18n) LoadFile(filename string) *I18n {
+func (i *i18n) LoadFile(filename string) *i18n {
 	data, err := os.ReadFile(filename)
 	if err != nil {
 		i.err = err
 		return i
 	}
-	var messages []Message
+	var messages []message
 	err = json.Unmarshal(data, &messages)
 	if err != nil {
 		i.err = err
@@ -47,7 +47,11 @@ func (i *I18n) LoadFile(filename string) *I18n {
 	return i
 }
 
-func (i *I18n) ToLang(lang string) *I18n {
+func (i *i18n) Lang() string {
+	return i.lang
+}
+
+func (i *i18n) ToLang(lang string) *i18n {
 	if i.err != nil || i.lang == lang {
 		return i
 	}
@@ -57,7 +61,7 @@ func (i *I18n) ToLang(lang string) *I18n {
 	return i
 }
 
-func (i *I18n) T(id string, args ...interface{}) string {
+func (i *i18n) T(id string, args ...interface{}) string {
 	i.mutex.RLock()
 	defer i.mutex.RUnlock()
 	if msg, ok := i.messages[id]; ok {
@@ -68,6 +72,6 @@ func (i *I18n) T(id string, args ...interface{}) string {
 	return id
 }
 
-func (i *I18n) Error() error {
+func (i *i18n) Error() error {
 	return i.err
 }
