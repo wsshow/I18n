28832e082d767e8316fa3988629d5b03b777d42c
ws <1479162130@qq.com>
2023-05-12 14:12:48 +0800

    test: i18n

---
+++ i18n_test.go
@@ -0,0 +1,62 @@
+package I18n
+
+import "testing"
+
+func TestI18n(t *testing.T) {
+
+	l := NewI18n().LoadFile("TestData/lang.json")
+	if err := l.Error(); err != nil {
+		t.Fatal(err)
+	}
+
+	// 默认语言
+	expected := "en"
+	actual := l.Lang()
+	if expected != actual {
+		t.Fatalf("expected: %s\nactual: %s\nerror:expected != actual", expected, actual)
+	}
+
+	expected = "core program was not found"
+	actual = l.T("core program was not found")
+	if expected != actual {
+		t.Fatalf("expected: %s\nactual: %s\nerror:expected != actual", expected, actual)
+	}
+
+	expected = "not found core module: i18n-core"
+	actual = l.T("not found core module: %s", "i18n-core")
+	if expected != actual {
+		t.Fatalf("expected: %s\nactual: %s\nerror:expected != actual", expected, actual)
+	}
+
+	expected = "current goroutine num: 3"
+	actual = l.T("current goroutine num: %d", 3)
+	if expected != actual {
+		t.Fatalf("expected: %s\nactual: %s\nerror:expected != actual", expected, actual)
+	}
+
+	// 切换语言
+	l.ToLang("zh")
+	expected = "zh"
+	actual = l.Lang()
+	if expected != actual {
+		t.Fatalf("expected: %s\nactual: %s\nerror:expected != actual", expected, actual)
+	}
+
+	expected = "未找到核心程序"
+	actual = l.T("core program was not found")
+	if expected != actual {
+		t.Fatalf("expected: %s\nactual: %s\nerror:expected != actual", expected, actual)
+	}
+
+	expected = "未找到核心模块：i18n-core"
+	actual = l.T("not found core module: %s", "i18n-core")
+	if expected != actual {
+		t.Fatalf("expected: %s\nactual: %s\nerror:expected != actual", expected, actual)
+	}
+
+	expected = "当前协程数：3"
+	actual = l.T("current goroutine num: %d", 3)
+	if expected != actual {
+		t.Fatalf("expected: %s\nactual: %s\nerror:expected != actual", expected, actual)
+	}
+}
