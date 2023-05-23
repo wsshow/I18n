f97919a9b2c1b9512ce75b0f5fad2797ff71c20f
ws <1479162130@qq.com>
2023-05-23 09:27:53 +0800

    feat: 添加GetLangs和GetGroups方法

--- i18n.go
+++ i18n.go
@@ -20,6 +20,8 @@ type messageGroup struct {
 type i18n struct {
 	messages map[string]map[string]message
 	mutex    sync.RWMutex
+	langs    []string
+	groups   []string
 	group    string
 	lang     string
 	err      error
@@ -34,6 +36,8 @@ func NewI18n() *i18n {
 }
 
 func (i *i18n) LoadFile(filename string) *i18n {
+	i.mutex.Lock()
+	defer i.mutex.Unlock()
 	data, err := os.ReadFile(filename)
 	if err != nil {
 		i.err = err
@@ -45,16 +49,29 @@ func (i *i18n) LoadFile(filename string) *i18n {
 		i.err = err
 		return i
 	}
-	i.mutex.Lock()
-	defer i.mutex.Unlock()
 	for _, group := range messageGroups {
 		mg := make(map[string]message)
 		for _, msg := range group.Messages {
 			mg[msg.ID] = msg
+			for lang := range msg.Langs {
+				if !i.InLangs(lang) {
+					i.langs = append(i.langs, lang)
+				}
+			}
 		}
 		i.messages[group.GroupName] = mg
+		i.groups = append(i.groups, group.GroupName)
 	}
 	return i
+}
+
+func (i *i18n) InLangs(lang string) bool {
+	for _, l := range i.langs {
+		if l == lang {
+			return true
+		}
+	}
+	return false
 }
 
 func (i *i18n) Lang() string {
@@ -84,6 +101,14 @@ func (i *i18n) ToGroup(group string) *i18n {
 		err:      i.err,
 	}
 	return n
+}
+
+func (i *i18n) GetLangs() []string {
+	return i.langs
+}
+
+func (i *i18n) GetGroups() []string {
+	return i.groups
 }
 
 func (i *i18n) T(id string, args ...interface{}) string {
